7d7de8c503767cc66b6dd5da14a416b1d9331ccc
Sergey-Volchkov <seriyvlastelin@gmail.com>
2017-01-18 18:36:32 +0300

    Добавили возможность выбора айпи и порта

---
+++ utils/data-emulator/client.py
@@ -0,0 +1,33 @@
+from socket import *
+import sys
+import telemetrySenderEmulator
+import time
+import params
+
+Parser = params.Parser()
+gen = telemetrySenderEmulator.generateData()
+argv = Parser.createParser()
+ip_and_port = argv.parse_args(sys.argv[1:])
+host = ip_and_port.ip
+port = int(ip_and_port.port)
+addr = (host, port)
+tcp_socket = socket(AF_INET, SOCK_STREAM)
+tcp_socket.connect(addr)
+
+while(True):
+    try:
+        data = gen.getMessage()
+        print("Gen data: ", data)
+        if not data:
+            tcp_socket.close()
+            sys.exit(1)
+        data = str.encode(data)
+        tcp_socket.send(data)
+        print(data)
+        time.sleep(0.5)
+    except KeyboardInterrupt:
+        print("\nCtr + C")
+        tcp_socket.close()
+        sys.exit(1)
+
+tcp_socket.close()
--- web/backend/app.py
+++ web/backend/app.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template
+import params
+import sys
+
+sys.path.append("../../utils/data-emulator")
 
 app = Flask(__name__)
-app.template_folder = '../frontend'
 
+app.template_folder = '../frontend'
+app.static_folder = "../frontend/static/"
+
+Parser = params.Parser()
+gen = telemetrySenderEmulator.generateData()
+argv = Parser.createParser()
+host = ip_and_port.ip
+port = int(ip_and_port.port)
+addr = (host, port)
 @app.route("/")
 def hello():
     return render_template('index.html')
+@app.route("/about.html")
+def about():
+    return render_template('index.html', name=about)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='{}'.format(host), port=port, debug=True)
