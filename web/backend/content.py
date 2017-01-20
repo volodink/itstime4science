01ed51f1918add7034410c0d3646556641acc2be
Olegas13 <oleg13000@gmail.com>
2017-01-21 00:56:48 +0300

    Обновлен раздел с сотрудниками. На каждой странице генерируется свой список людей

--- web/backend/content.py
+++ web/backend/content.py
@@ -1,18 +1,104 @@
-def setIMG(*tuplePhoto):
+def setGalleryImg(*tuplePhoto):
 
 	return tuple((count, tuplePhoto[count]) for count in range(len(tuplePhoto))) 
+
+def setTemaData(*tupleData):
+
+	return dict({"name":tupleData})
 
 
 
 def getContent():
 
+	volodink = {"name": "Константин Володин",
+				"link_to_photo": "https://pp.vk.me/c638023/v638023262/1bc35/KO9Y64lP6io.jpg",
+				"job_title": "Научный руководитель",
+				"comment": ""}
+
+	kulikovo = {"name": "Олег Куликов",
+				"link_to_photo": "http://placehold.it/200x200",
+				"job_title": "Технический консультант",
+				"comment": ""}
+
+	garkaeva = {"name": "Александр Гарькаев",
+				"link_to_photo": "http://placehold.it/200x200",
+				"job_title": "Лаборант",
+				"comment": ""}
+
+	stramovd = {"name": "Дмитрий Страмов",
+				"link_to_photo": "https://pp.vk.me/c638023/v638023262/1bc4b/slIt41ns4jY.jpg",
+				"job_title": "Лаборант",
+				"comment": ""}
+
+	vavilova = {"name": "Андрей Вавилов",
+				"link_to_photo": "http://placehold.it/200x200",
+				"job_title": "Лаборант",
+				"comment": ""}
+
+	caplinm = {"name": "Михаил Цаплин",
+				"link_to_photo": "http://placehold.it/200x200",
+				"job_title": "Лаборант",
+				"comment": ""}
+
+	baikova = {"name": "Антон Байков",
+				"link_to_photo": "http://placehold.it/200x200",
+				"job_title": "Лаборант",
+				"comment": ""}
+
+	volckovs = {"name": "Сергей Волчков",
+				"link_to_photo": "http://placehold.it/200x200",
+				"job_title": "Лаборант",
+				"comment": ""}
+
+	kopylovv = {"name": "Вадим Копылов",
+				"link_to_photo": "http://placehold.it/200x200",
+				"job_title": "Лаборант",
+				"comment": ""}
+
+	kolginp = {"name": "Паша Колодкин",
+				"link_to_photo": "http://placehold.it/200x200",
+				"job_title": "Лаборант",
+				"comment": ""}
+
+	volkova = {"name": "Александр Волков",
+				"link_to_photo": "https://pp.vk.me/c638023/v638023262/1bc85/PNEW2q7TdbQ.jpg",
+				"job_title": "Лаборант",
+				"comment": ""}
+
+	kedelidzeo = {"name": "Кеделидзе Олег",
+				"link_to_photo": "https://pp.vk.me/c638023/v638023262/1bc44/5OsS6C-RmQ8.jpg",
+				"job_title": "Лаборант",
+				"comment": ""}
+
+	mesherinovae =  {"name": "Евгения Мещеринова",
+				"link_to_photo": "https://pp.vk.me/c638023/v638023262/1bc8c/d83ROGuf4Pc.jpg",
+				"job_title": "Лаборант",
+				"comment": ""}
+
+
+
+
 	main = {"title": "itstime4science | Главная",
 			"head_photo": "https://pp.vk.me/c637528/v637528262/1976f/rx5wxLsuZyQ.jpg",
 			"head_title": "ВВиВС",
 			"head_footer": "Добро пожаловать на интернет-портал лаборатории ВВиВС (ПензГТУ)!",
-			"gallery": setIMG("https://pp.vk.me/c636323/v636323235/44666/CYzELG7-Lf0.jpg",
+			"gallery": setGalleryImg("https://pp.vk.me/c636323/v636323235/44666/CYzELG7-Lf0.jpg",
 					 		  "https://pp.vk.me/c636323/v636323235/449d1/zsmPgBHN2yM.jpg",
-					          "https://pp.vk.me/c636323/v636323235/449ec/EJ6mhheOnXQ.jpg")
+					          "https://pp.vk.me/c636323/v636323235/449ec/EJ6mhheOnXQ.jpg"),
+			"team_photo": {"team_row_title": "Сотрудники лаборатории ВВиВС",
+							"people": (volodink,
+										kulikovo,
+										garkaeva,
+										kedelidzeo,
+										stramovd,
+										mesherinovae,
+										vavilova,
+										caplinm,
+										baikova,
+										volckovs,
+										kopylovv,
+										kolginp,
+										volkova)}
 			}
 
 
@@ -20,9 +106,15 @@ def getContent():
 			  "head_photo": "https://pp.vk.me/c637528/v637528262/19735/c7J0oC93950.jpg",
 			  "head_title": "Коптер",
 			  "head_footer": "Тут будет текст",
-			  "gallery": setIMG("https://pp.vk.me/c636323/v636323235/43d6f/HfCDSO1iE_w.jpg",
+			  "gallery": setGalleryImg("https://pp.vk.me/c636323/v636323235/43d6f/HfCDSO1iE_w.jpg",
 								"https://pp.vk.me/c836227/v836227262/1db5d/Oao-irsb8Sc.jpg",
-								"https://pp.vk.me/c636323/v636323235/44708/oaaCdr02RSU.jpg")
+								"https://pp.vk.me/c636323/v636323235/44708/oaaCdr02RSU.jpg"),
+			  "team_photo": {"team_row_title": 'Команда "Копер"',
+							"people": (kulikovo,
+										garkaeva,
+										stramovd,
+										vavilova,					
+										baikova)}
 			}
 
 	
@@ -30,8 +122,17 @@ def getContent():
 				 "head_photo": "http://www.roscosmos.ru/media/gallery/big/20544/744950758.jpg",
 				 "head_title": "Студенческая лига",
 				 "head_footer": "Тут будет текст",
-				 "gallery": setIMG("https://i.ytimg.com/vi/SXW5eAjPD2U/maxresdefault.jpg",
-				 				   "http://i.ytimg.com/vi/teArassP_E4/maxresdefault.jpg")}
+				 "gallery": setGalleryImg("https://i.ytimg.com/vi/SXW5eAjPD2U/maxresdefault.jpg",
+				 				   "http://i.ytimg.com/vi/teArassP_E4/maxresdefault.jpg"),
+				 "team_photo": {"team_row_title": 'Команда "Спутник"',
+							"people": (kulikovo,
+										garkaeva,
+										kedelidzeo,
+										caplinm,
+										volckovs,
+										kopylovv,
+										kolginp)}
+				}
 	
 
 	return {"main": main,
