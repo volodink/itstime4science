c4536c31cd8307ddd66373ce5d6bc257613276d0
Sergey-Volchkov <seriyvlastelin@gmail.com>
2017-03-27 23:36:20 +0300

    немного доделал клиент

--- test_client/client.py
+++ test_client/client.py
@@ -10,28 +10,19 @@ argv = Parser.createParser()
 ip_and_port = argv.parse_args(sys.argv[1:])
 #host = ip_and_port.ip
 #port = int(ip_and_port.port)
-host = "server"
+host = "0.0.0.0"
 port = 5100
 print(host,port)
 
 addr = (host, port)
 tcp_socket = socket(AF_INET, SOCK_STREAM)
 tcp_socket.connect(addr)
-
-while(True):
-    try:
-        data = gen.getMessage()
-        print("Gen data: ", data)
-        if not data:
-            tcp_socket.close()
-            sys.exit(1)
-        data = str.encode(data)
-        tcp_socket.send(data)
-        print(data)
-        time.sleep(0.5)
-    except KeyboardInterrupt:
-        print("\nCtr + C")
-        tcp_socket.close()
-        sys.exit(1)
+file = open("binary.file", "rb")
+while not file.eof:
+    buff = file.read(89)  # считать 1024 байт
+    string = base64.b64encode(buff)  # base64 кодирование
+    buff = str.encode(buff) # Вот это под вопросом, перед этим вместо buff  на обоих местах стояла data
+    tcp_socket.send(file)
+    print('Data has been sent')
 
 tcp_socket.close()
