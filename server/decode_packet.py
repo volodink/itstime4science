242b70acc722c376808be44d17d53f5cd27c63cf
Sergey-Volchkov <seriyvlastelin@gmail.com>
2017-04-14 18:15:00 +0300

    Почти настроил вывод в базу данных, нужно дофиксить

--- server/decode_packet.py
+++ server/decode_packet.py
@@ -1,8 +1,13 @@
+from datetime import datetime
 import struct
+import MySQLdb
 
 f = open('packet.bin', 'rb')
 SYT=f.read(3)
 print(SYT)
+def packet(*args,i):
+    return args[i]
+
 def clear_string(str):
     new_str = ''
     for s in str:
@@ -35,12 +40,20 @@ def run(number):
     print(len(toBitArray(number)))
     return toBitArray(number)
 
-
+i=0
 d = [] #time
+
+data = 10001
+d.append(data)
 
 data = read_data(f, 4)#time
 data =int.from_bytes(data, byteorder='little')
+print(datetime.fromtimestamp(data))
+data = datetime.fromtimestamp(data)
+print(str(data))
+data = str(data)
 d.append(data)
+
 
 data = read_data(f, 4) #lat
 data = struct.unpack('f',data)
@@ -131,7 +144,6 @@ data= struct.unpack('f',data)
 d.append (clear_string(data))
 
 data = read_data(f, 1)#ozone
-print(bool(data))
 d.append(bool(data))
 
 data = read_data(f, 4)#status
@@ -140,9 +152,20 @@ l = run(databin)
 d.append(l)
 
 
+
 f = open('formated_dates.txt', 'w')
 print(d[0:24])
 for i in range(len(d)):
     f.write(str(d[i]) + '    ')
-f.close()
+    print(str(i)+ '-' + str(d[i]))
+print(i)
+db = MySQLdb.connect(host="localhost", user="root", passwd="s21021999s", db="packet", charset='utf8')
+cursor = db.cursor()
+cursor.execute =("""INSERT INTO gprs(numbersOfFlight, datatime, lat, lon,alt,temp1,temp1,pressure1,pressure2,bat_crg,bat_volt,bat_temp,vect_axel1,vect_axel2,ultraviolet1,ultraviolen2,infrared1,infrared2,hdop,vdop,sats,radiation,dust,ozone,status) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,)""",(d[0],d[1],d[2],d[3],d[4],d[5],d[6],d[7],d[8],d[9],d[10],d[11],d[12],d[13],d[14],d[15],d[16],d[17],d[18],d[19],d[20],d[21],d[22],d[23],d[24]))
+#cursor.execute("""SELECT * FROM gprs;""")
+# except:
+#     db.rollback()
+db.commit()
+db.close()
+#data =  cursor.fetchall()
 # # ГЛЯНУТЬ ЕМЕТ
