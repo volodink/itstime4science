41d5a0d84fd22e1737c9b4970eda81ddd3cd5545
Sergey-Volchkov <seriyvlastelin@gmail.com>
2017-05-01 22:57:25 +0300

    Настроил сервер на приём данных без перезапуска сервера. Данные берутся из файла(если нужно ввести данные самому, раскоментируйте пару строк в server.py

--- gprs/server.py
+++ gprs/server.py
@@ -1,5 +1,5 @@
 from socket import *
-#from modules import decode_packet
+from modules import decode_packet
 import sys
 from modules import params
 
@@ -9,67 +9,27 @@ ip_and_port = argv.parse_args(sys.argv[1:])
 #host = ip_and_port.ip
 #port = int(ip_and_port.port)
 host = "0.0.0.0"
-port = 5100
+port = 5300
 addr = (host, port)
 print(host,port)
 tcp_socket = socket(AF_INET, SOCK_STREAM)
 tcp_socket.bind(addr)
-tcp_socket.listen(1)
-f = open('logs/gprs.log', 'a+')
-while True:
-    # tcp_socket.settimeout(5)
+tcp_socket.listen(10)
+loop = True
+while loop:
+    data = None
     print('wait connection...')
     conn, addr = tcp_socket.accept()
-    data = conn.recv(109)
-    #decode_packet.insert(data)
-    print(data)
-    if data:
-        f.write(str(data))
-    else:
-        break
-
-f.close()
+    while loop:
+        f = open('logs/gprs.log', 'a+')
+        data = conn.recv(109)
+        decode_packet.insert(data)
+        print(data)
+        if data:
+            f.write(str(data))
+            f.close()
+        else:
+            f.close()
+            break
 conn.close()
 tcp_socket.close()
-
-# from socket import *
-# import params
-# import decode_packet
-# import sys
-# import asyncio
-#
-# def event_handler(loop, stop=False):
-#     print('Вызван обработчик события')
-#     if stop:
-#         print('Цикл останавливается')
-#         loop.stop()
-# if __name__ == '__main__':
-#     loop = asyncio.get_event_loop()
-#     try:
-#         loop.call_later(1, event_handler, loop)
-#         Parser = params.Parser()
-#         argv = Parser.createParser()
-#         ip_and_port = argv.parse_args(sys.argv[1:])
-#         #host = ip_and_port.ip
-#         #port = int(ip_and_port.port)
-#         host = "localhost"
-#         port = 5100
-#         addr = (host, port)
-#         print(host,port)
-#         tcp_socket = socket(AF_INET, SOCK_STREAM)
-#         tcp_socket.bind(addr)
-#         tcp_socket.listen(1)
-#         f = open('packet.bin', 'rb')
-#         while True:
-#             print('wait connection...')
-#             conn, addr = tcp_socket.accept()
-#             data = conn.recv(109)
-#             print(data)
-#             f.write(data)
-#             f.close()
-#             conn.close()
-#
-#         loop.run_forever()
-#     finally:
-#         loop.close()
-#         tcp_socket.close()
