3559edaf418c4b49fae8432b8b6631f8c2df59bb
Sergey-Volchkov <seriyvlastelin@gmail.com>
2017-05-02 16:55:59 +0300

    Фикс

--- gprs/server.py
+++ gprs/server.py
@@ -1,33 +1,22 @@
 from socket import *
 from modules import decode_packet
-import sys
-from modules import params
 
-
-Parser = params.Parser()
-argv = Parser.createParser()
-ip_and_port = argv.parse_args(sys.argv[1:])
-#host = ip_and_port.ip
-#port = int(ip_and_port.port)
-host = "0.0.0.0"
-port = 5100
-addr = (host, port)
-print(host,port)
 tcp_socket = socket(AF_INET, SOCK_STREAM)
-tcp_socket.bind(addr)
+tcp_socket.bind(('',5100))
 tcp_socket.listen(10)
-
-loop = True
-while loop:
+while True:
     print('wait connection to ')
     conn, addr = tcp_socket.accept()
-    while loop:
+    while True:
         f = open('logs/gprs.log', 'a+')
         data = conn.recv(109)
-        decode_packet.insert(data)
-        print(data)
-        f.write(str(data))
-        f.close()
+        if data:
+            decode_packet.insert(data)
+            print(data)
+            f.write(str(data))
+            f.close()
+        if not data:
+            break
 print('Connection is close')
 conn.close()
 tcp_socket.close()
