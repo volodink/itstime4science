dfe1da4eb34c16c0029b87c14a441477358c2778
volodink <volodin.konstantin@gmail.com>
2017-05-19 01:40:57 +0300

    Fixing routes.

--- web/backend/app.py
+++ web/backend/app.py
@@ -3,7 +3,7 @@ import os
 import sys
 import urllib.request
 
-from flask import Flask
+from flask import Flask, render_template
 from flask.ext.mysql import MySQL
 from flask_socketio import SocketIO
 from flask_socketio import emit
@@ -32,8 +32,49 @@ sys.path.append(str(os.path.abspath(sys.argv[0])))
 
 data = content.getContent()
 
-from modules.flask_routes import *
-from modules.socketio_routes import *
+
+@app.route("/")
+def main():
+    return render_template('index.html', is_dev=is_dev, data = data["main"], main = True)
+
+
+@app.route("/copter")
+def copter():
+    return render_template('index.html', is_dev=is_dev, data = data["copter"])
+
+
+@app.route("/satellite")
+def satellite():
+    return render_template('index.html', is_dev=is_dev, data = data["satellite"])
+
+
+@app.route("/mcc")
+def mcc():
+    return render_template('mcc.html', is_dev=is_dev, panel_tags=content.panel_tags, communication_channel_panel=content.communication_channel_panel)
+
+
+@socketio.on('my_event', namespace='/mcc')
+def message(message):
+        json_data = parsing.getData(mysql)
+        mas = json.loads(json_data)
+        if message['data'] != mas[0]['id']:
+            emit('packet', {'json_data': json_data}, namespace='/mcc')
+        else:
+            emit('packet', {'json_data': 0}, namespace='/mcc')
+
+
+@socketio.on('my_event2', namespace='/mcc')
+def message():
+    response = urllib.request.urlopen('https://api.aprs.fi/api/get?name=UB4FEU-11&what=loc&apikey=API_KEY&format=json')
+    emit('aprs', {'response': response.read()}, namespace='/mcc')
+
+@socketio.on('last_dots', namespace='/mcc')
+def msg():
+    #количество последних маркеров, которые будут показываться при загрузке страницы
+    i = 10
+    json_data = parsing.last_dots(mysql, i)
+    emit('lastMarkers', {'json_data': json_data}, namespace='/mcc')
+
 
 if __name__ == '__main__':
     mysql.init_app(app)
