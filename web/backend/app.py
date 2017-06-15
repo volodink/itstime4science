09b78610dd71686a7ed208a39d5b8a5597727233
sergey-volchkov <seriyvlastelin@gmail.com>
2017-06-15 15:49:00 +0300

    Добавил либу

--- web/backend/app.py
+++ web/backend/app.py
@@ -10,7 +10,7 @@ from flask_socketio import send, emit
 import json
 import aprslib
 import urllib.request
-from modules import decode_aprs
+#from modules import decode_aprs
 
 app = Flask(__name__)
 app.config['MYSQL_DATABASE_HOST'] = os.getenv("MYSQL_DATABASE_HOST", "0")
