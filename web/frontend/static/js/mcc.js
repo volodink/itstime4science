6c01f249e53bca11e500e3689fb26beecdd5fcfe
sergey-volchkov <seriyvlastelin@gmail.com>
2017-07-01 15:54:50 +0300

    fix

--- web/frontend/static/js/mcc.js
+++ web/frontend/static/js/mcc.js
@@ -108,7 +108,7 @@ $("#aprs").change(function(){
 function APRS(msg,markers_aprs){
 
 	if (msg['json_data'] != 0){
-	    if (msg['json_data'] != 'nothing'){
+
             change_data(msg['type'],k);
 		    var json_packet = msg['json_data'];
 		    var json = JSON.parse(json_packet);
@@ -132,9 +132,7 @@ function APRS(msg,markers_aprs){
                         socket.emit('my_even2',{data: j.id});
 
 			    })
-          }
-            else {console.log("Данных апрс в базе данных нет, хозяина");
-                                socket.emit('my_event2',{data: 0});}
+          
 	}   
     else{
         console.log("Данных апрс нема, хозяина");
@@ -144,41 +142,39 @@ function APRS(msg,markers_aprs){
 }
 function GPRS(msg,markers_gprs){
 					if (msg['json_data'] != 0){
-                                    	    if (msg['json_data'] != 'nothing'){
-						                                        var json_packet = msg['json_data'];
-						                                        var json = JSON.parse(json_packet);
-						                                        json.forEach(function (item, i, json) {
-									                                            j = json[i];
-									                                            lat = j.lat;
-									                                            lon = j.lon;
-									                                            $('#id').html(j.id);
-									                                            $('#numberOfFlight').html(j.numberOfFlight);
-									                                            $('#datetime').html(j.datetime);
-									                                            $('#lat').html(j.lat);
-									                                            $('#lon').html(j.lon);
-									                                            $('#alt').html(j.alt);
-									                                            $('#temp1').html(j.temp1);
-									                                            $('#temp2').html(j.temp2);
-									                                            $('#pressure1').html(j.pressure1);
-									                                            $('#pressure2').html(j.pressure2);
-									                                            $('#bat_volt').html(j.bat_volt);
-									                                            $('#vect_axel1x').html(j.vect_axel1x);
-									                                            $('#vect_axel1y').html(j.vect_axel1y);
-									                                            $('#vect_axel1z').html(j.vect_axel1z);
-									                                            $('#hdop').html(j.hdop);
-									                                            $('#vdop').html(j.vdop);
-									                                            $('#sats').html(j.sats);
-									                                            $('#radiation').html(j.radiation);
-                                                                                change_data(msg['type'],k);
-									                                            var position = {lat: parseFloat(lat), lng: parseFloat(lon)};
-										
-									                                            addMarker(position,map,lat,lon,markers_gprs,'gprs');
-									                                            console.log("Данные гпрс есть, омномном");
-                                                                                socket.emit('my_event',{data: j.id});
-									                                        })
-                                            }
-                                            else {console.log("Данных гпрс в базе данных нет, хозяина");
-                                                socket.emit('my_event',{data: 0});}
+
+                            var json_packet = msg['json_data'];
+                            var json = JSON.parse(json_packet);
+                            json.forEach(function (item, i, json) {
+                                            j = json[i];
+                                            lat = j.lat;
+                                            lon = j.lon;
+                                            $('#id').html(j.id);
+                                            $('#numberOfFlight').html(j.numberOfFlight);
+                                            $('#datetime').html(j.datetime);
+                                            $('#lat').html(j.lat);
+                                            $('#lon').html(j.lon);
+                                            $('#alt').html(j.alt);
+                                            $('#temp1').html(j.temp1);
+                                            $('#temp2').html(j.temp2);
+                                            $('#pressure1').html(j.pressure1);
+                                            $('#pressure2').html(j.pressure2);
+                                            $('#bat_volt').html(j.bat_volt);
+                                            $('#vect_axel1x').html(j.vect_axel1x);
+                                            $('#vect_axel1y').html(j.vect_axel1y);
+                                            $('#vect_axel1z').html(j.vect_axel1z);
+                                            $('#hdop').html(j.hdop);
+                                            $('#vdop').html(j.vdop);
+                                            $('#sats').html(j.sats);
+                                            $('#radiation').html(j.radiation);
+                                            change_data(msg['type'],k);
+                                            var position = {lat: parseFloat(lat), lng: parseFloat(lon)};
+	
+                                            addMarker(position,map,lat,lon,markers_gprs,'gprs');
+                                            console.log("Данные гпрс есть, омномном");
+                                            socket.emit('my_event',{data: j.id});
+                                        })
+
 					}    
 			        console.log("Данных гпрс нема, хозяина");
                     socket.emit('my_event',{data: j.id});
@@ -187,41 +183,38 @@ function GPRS(msg,markers_gprs){
                             };
 function TELEMETRY(msg,markers_gprs){
 					if (msg['json_data'] != 0){
-                                    	    if (msg['json_data'] != 'nothing'){
-						                                        var json_packet = msg['json_data'];
-						                                        var json = JSON.parse(json_packet);
-						                                        json.forEach(function (item, i, json) {
-									                                            j = json[i];
-									                                            lat = j.lat;
-									                                            lon = j.lon;
-									                                            $('#id').html(j.id);
-									                                            $('#numberOfFlight').html(j.numberOfFlight);
-									                                            $('#datetime').html(j.datetime);
-									                                            $('#lat').html(j.lat);
-									                                            $('#lon').html(j.lon);
-									                                            $('#alt').html(j.alt);
-									                                            $('#temp1').html(j.temp1);
-									                                            $('#temp2').html(j.temp2);
-									                                            $('#pressure1').html(j.pressure1);
-									                                            $('#pressure2').html(j.pressure2);
-									                                            $('#bat_volt').html(j.bat_volt);
-									                                            $('#vect_axel1x').html(j.vect_axel1x);
-									                                            $('#vect_axel1y').html(j.vect_axel1y);
-									                                            $('#vect_axel1z').html(j.vect_axel1z);
-									                                            $('#hdop').html(j.hdop);
-									                                            $('#vdop').html(j.vdop);
-									                                            $('#sats').html(j.sats);
-									                                            $('#radiation').html(j.radiation);
-                                                                                change_data(msg['type'],k);
-									                                            var position = {lat: parseFloat(lat), lng: parseFloat(lon)};
-										
-									                                            addMarker(position,map,lat,lon,markers_gprs,'gprs');
-									                                            console.log("Данные телеметрии есть, омномном");
-                                                                                socket.emit('my_event3',{data: j.id});
-									                                        })
-                                            }
-                                            else {console.log("Данных телеметрии в базе данных нет, хозяина");
-                                                socket.emit('my_event3',{data: 0});}
+                            var json_packet = msg['json_data'];
+                            var json = JSON.parse(json_packet);
+                            json.forEach(function (item, i, json) {
+                                            j = json[i];
+                                            lat = j.lat;
+                                            lon = j.lon;
+                                            $('#id').html(j.id);
+                                            $('#numberOfFlight').html(j.numberOfFlight);
+                                            $('#datetime').html(j.datetime);
+                                            $('#lat').html(j.lat);
+                                            $('#lon').html(j.lon);
+                                            $('#alt').html(j.alt);
+                                            $('#temp1').html(j.temp1);
+                                            $('#temp2').html(j.temp2);
+                                            $('#pressure1').html(j.pressure1);
+                                            $('#pressure2').html(j.pressure2);
+                                            $('#bat_volt').html(j.bat_volt);
+                                            $('#vect_axel1x').html(j.vect_axel1x);
+                                            $('#vect_axel1y').html(j.vect_axel1y);
+                                            $('#vect_axel1z').html(j.vect_axel1z);
+                                            $('#hdop').html(j.hdop);
+                                            $('#vdop').html(j.vdop);
+                                            $('#sats').html(j.sats);
+                                            $('#radiation').html(j.radiation);
+                                            change_data(msg['type'],k);
+                                            var position = {lat: parseFloat(lat), lng: parseFloat(lon)};
+	
+                                            addMarker(position,map,lat,lon,markers_gprs,'gprs');
+                                            console.log("Данные телеметрии есть, омномном");
+                                            socket.emit('my_event3',{data: j.id});
+                                        });
+
 					}    
 			        console.log("Данных телеметрии нема, хозяина");
                     socket.emit('my_event3',{data: j.id});
