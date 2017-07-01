99f2fbcd85557aa003867b21c3e932c8079e5c21
sergey-volchkov <seriyvlastelin@gmail.com>
2017-07-01 10:39:46 +0300

    1

--- web/backend/modules/parsing.py
+++ web/backend/modules/parsing.py
@@ -75,7 +75,65 @@ def pars_gprs(mysql):
         r.append(e)
 
         return json.dumps(r)
+def pars_telemetry(mysql):
+    cur = mysql.connect().cursor()
+    cur.execute("select * from telemetry ORDER BY id DESC LIMIT 1")
+    data = list(cur.fetchall())
+    mysql.connect().commit
+    r = []
+    for element in data:
+        e = dict()
+        e['id'] = element[0]
+        e['numberOfFlight'] = element[1]
+        e['datetime'] = str(element[2])
+        e['lat'] = element[3]
+        e['lon'] = element[4]
+        e['alt'] = element[5]
+        e['temp1'] = element[6]
+        e['temp2'] = element[7]
+        e['pressure1'] = element[8]
+        e['pressure2'] = element[9]
+        e['bat_volt'] = element[10]
+        e['vect_axel1x'] = element[11]
+        e['vect_axel1y'] = element[12]
+        e['vect_axel1z'] = element[13]
+        e['ultraviolet1'] = element[14]
+        e['ultraviolet2'] = element[15]
+        e['infrared1'] = element[16]
+        e['infrared2'] = element[17]
+        e['hdop'] = element[18]
+        e['vdop'] = element[19]
+        e['sats'] = element[20]
+        e['radiation'] = element[21]
+        e['dust'] = element[22]
+        e['ozone'] = element[23]
+        e['status'] = dict()
 
+        s = element[24].split(',')
+        e['status']['datetime'] = s[0]
+        e['status']['lat'] = s[1]
+        e['status']['lon'] = s[1]
+        e['status']['alt'] = s[1]
+        e['status']['temp1'] = s[2]
+        e['status']['temp2'] = s[3]
+        e['status']['pressure1'] = s[4]
+        e['status']['pressure2'] = s[5]
+        e['status']['bat_volt'] = s[6]
+        e['status']['vect_axel1x'] = s[7]
+        e['status']['vect_axel1y'] = s[7]
+        e['status']['vect_axel1z'] = s[7]
+        e['status']['ultraviolet1'] = s[8]
+        e['status']['ultraviolet2'] = s[9]
+        e['status']['infrared1'] = s[10]
+        e['status']['infrared2'] = s[11]
+        e['status']['hdop'] = s[12]
+        e['status']['vdop'] = s[13]
+        e['status']['sats'] = s[14]
+        e['status']['radiation'] = s[15]
+        e['status']['ozone'] = s[16]
+        r.append(e)
+
+        return json.dumps(r)
 
 def pars_aprs(mysql):
     cur = mysql.connect().cursor()
